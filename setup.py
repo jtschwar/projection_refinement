d0b6637a95727e61c67242f6887739e65859d276
Peter Ercius <percius@lbl.gov>
2024-09-17 12:58:52 -0700

    fix typo

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
 
-    install_requires=['numpy<', 'scipy', 'matplotlib', 'h5py>=3'],
+    install_requires=['numpy', 'scipy', 'matplotlib', 'h5py>=3'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
